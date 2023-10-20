0818d40a31ae9584f2de5b25bcd6e5359b70e72f
James Chellew <JamesPChellew@outlook.com>
2023-10-20 10:55:45 +0800

    Add project files.

---
+++ Drone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DroneSerivceForm
+{
+    internal class Drone
+    {
+        string name;
+        string model;
+        string problem;
+        string cost;
+        int tag;
+    }
+}
