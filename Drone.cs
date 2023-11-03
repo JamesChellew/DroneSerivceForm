37e0ff42c9789152db696782f8bc0b89b758b7e2
James Chellew <JamesPChellew@outlook.com>
2023-11-03 11:01:38 +0800

    Foundations of Drone class completed.

--- Drone.cs
+++ Drone.cs
@@ -8,10 +8,76 @@ namespace DroneSerivceForm
 {
     internal class Drone
     {
-        string name;
-        string model;
-        string problem;
-        string cost;
-        int tag;
+        private string _name;
+        private string _model;
+        private string _issue;
+        private double _cost;
+        private int _tag;
+
+        public string GetName()
+        {
+            return _name;
+        }
+        public void SetName(string name)
+        {
+            if (!string.IsNullOrEmpty(name))
+            {
+                _name = name;
+            }
+            else
+            {
+                _name = "Unspecified";
+            }
+        }
+
+        public string GetModel()
+        {
+            return _model;
+        }
+        public void SetModel(string model)
+        {
+            if (!string.IsNullOrEmpty(model))
+            {
+                _model = model;
+            }
+            else
+            {
+                _model = "Unspecified";
+            }
+        }
+
+        public string GetIssue()
+        {
+            return _issue;
+        }
+        public void SetIssue(string issue) 
+        {
+            if (!string.IsNullOrEmpty(_issue))
+            {
+                _issue = issue;
+            }
+            else
+            {
+                _issue = "Unspecified";
+            }
+        }
+
+        public double GetCost()
+        {
+            return _cost;
+        }
+        public void SetCost(double cost)
+        {
+            _cost = cost;
+        }
+
+        public int GetTag()
+        {
+            return _tag;
+        }
+        public void SetTag(int tag)
+        {
+            _tag = tag;
+        }
     }
 }
